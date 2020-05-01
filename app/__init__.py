29d69d9a67f1d8b5fb781e6b8f8ca4ab661ddf6b
barackmaund1 <pmaundu49@gmail.co>
2020-05-01 22:46:06 +0300

    created extentions and configarations

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    # Initializing application
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    
+    
+    # Initializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+     
+    return app
